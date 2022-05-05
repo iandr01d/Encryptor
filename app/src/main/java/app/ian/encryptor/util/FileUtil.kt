0de808d5b80ffc3218e873f7127dbb9554888798
Ian Eng <khiawyen@gmail.com>
2022-05-05 20:44:47 +0800

    Read and parse json files from assets

---
+++ app/src/main/java/app/ian/encryptor/util/FileUtil.kt
@@ -0,0 +1,17 @@
+package app.ian.encryptor.util
+
+import android.content.Context
+import java.io.IOException
+
+object FileUtil {
+    fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+        val jsonString: String
+        try {
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        return jsonString
+    }
+}
