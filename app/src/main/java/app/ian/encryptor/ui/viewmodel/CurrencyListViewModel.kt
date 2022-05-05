25229c5e9983ff466a74d41c6a79e1fa6d15ba3c
Ian Eng <khiawyen@gmail.com>
2022-05-05 21:21:15 +0800

    Create CurrencyListViewModel and inject CurrencyInfoRepository as its dependency

---
+++ app/src/main/java/app/ian/encryptor/ui/viewmodel/CurrencyListViewModel.kt
@@ -0,0 +1,51 @@
+package app.ian.encryptor.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.ian.encryptor.data.CurrencyInfoRepository
+import app.ian.encryptor.model.CurrencyInfo
+import app.ian.encryptor.model.CurrencyInfoUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencyListViewModel @Inject constructor(
+    private val repository: CurrencyInfoRepository
+) : ViewModel() {
+
+    private var getAllCurrenciesJob: Job? = null
+    private var insertAllCurrenciesJob: Job? = null
+    private var deleteAllCurrenciesJob: Job? = null
+
+    private val _uiState = MutableStateFlow(CurrencyInfoUiState(listOf()))
+    val uiState: StateFlow<CurrencyInfoUiState> = _uiState.asStateFlow()
+
+    fun getAllCurrencies(shouldSort: Boolean) {
+        getAllCurrenciesJob?.cancel()
+        getAllCurrenciesJob = viewModelScope.launch {
+            repository.getAllCurrencies(shouldSort).collect {
+                _uiState.update { uiState ->
+                    uiState.copy(currencyInfoList = it)
+                }
+            }
+        }
+    }
+
+    suspend fun insertAllCurrencies(currencies: List<CurrencyInfo>) {
+        insertAllCurrenciesJob?.cancel()
+        insertAllCurrenciesJob = viewModelScope.launch {
+            repository.insertAllCurrencies(currencies)
+                .also { getAllCurrencies(false) }
+        }
+    }
+
+    suspend fun deleteAllCurrencies() {
+        deleteAllCurrenciesJob?.cancel()
+        deleteAllCurrenciesJob = viewModelScope.launch {
+            repository.deleteAllCurrencies()
+        }
+    }
+}
